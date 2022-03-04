8738fdb5e9aa7d8d541a2067a90efd6a98cc42b4
Rahul Teja <rahulteja.a@hotmail.com>
2022-03-04 10:23:02 +0530

    Added React Spring

--- src/Exposed/Container/index.tsx
+++ src/Exposed/Container/index.tsx
@@ -1,10 +1,11 @@
-import { ReactChild, ReactChildren } from 'react'
+import { ReactChild, ReactChildren, useEffect, useState } from 'react'
 import EmailConnect from '../../Components/EmailConnect'
 import Footer from '../../Components/Footer'
 import Header from '../../Components/Header'
 import SocialConnect from '../../Components/SocialConnect'
 import GlobalStyles from '../../Styles/GlobalStyles'
 import { MainSection } from './style'
+import { useTransition, animated } from 'react-spring'
 
 interface HolderProps {
 	children: ReactChild | ReactChildren
@@ -12,24 +13,39 @@ interface HolderProps {
 }
 
 const Container = ({ children, label }: HolderProps) => {
-	// const device = useDeviceType()
-	// useEffect(() => {
-	// 	document.body.dataset.device = device
-	// }, [device])
+	const [isLoading, setIsLoading] = useState(true)
+	useEffect(() => {
+		setIsLoading(false)
+	}, [])
+
+	const transition = useTransition(isLoading, {
+		config: { mass: 1, tension: 80, friction: 18 },
+		from: { x: 0, y: 600, opacity: 0, delay: 400 },
+		enter: { x: 0, y: 0, opacity: 1, delay: 400 },
+		leave: { x: 0, y: 600, opacity: 0, delay: 400 },
+	})
 
 	return (
 		<>
 			<GlobalStyles />
 			<div>
 				<Header label={label} />
-				<SocialConnect />
-				<EmailConnect />
-				<div id="content">
-					<MainSection>
-						<section>{children}</section>
-					</MainSection>
-					<Footer />
-				</div>
+				{transition((style: any, item: any) =>
+					!item ? (
+						<animated.div style={style}>
+							<SocialConnect />
+							<EmailConnect />
+							<div id="content">
+								<MainSection>
+									<section>{children}</section>
+								</MainSection>
+								<Footer />
+							</div>
+						</animated.div>
+					) : (
+						''
+					)
+				)}
 			</div>
 		</>
 	)
